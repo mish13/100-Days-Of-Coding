4c7cc3740a31a5f9deb0d1dee9a364431da545ad
A. K. M. Fakhrul Hossain <ahmishal90@gmail.com>
2019-10-16 12:46:33 +0600

    Create 40.Rotate Image.cpp

---
+++ Leetcode/40.Rotate Image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        vector <vector <int> > result;
+        for(int j=0; j<matrix.size();j++)
+        {
+            vector <int> tmp;
+            for(int i=matrix[j].size()-1; i>=0;i--)
+            {
+                tmp.push_back(matrix[i][j]);
+            }
+            result.push_back(tmp);
+        }
+        for(int i=0; i<matrix.size();i++)
+        {
+            vector <int> tmp;
+            for(int j=0; j<matrix[i].size();j++)
+            {
+                matrix[i][j]=result[i][j];
+            }
+        }
+    }
+};
