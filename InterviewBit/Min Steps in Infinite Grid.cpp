c0e5697d37ab6c9701a1b561184eee6db4c00ca6
A. K. M. Fakhrul Hossain <ahmishal90@gmail.com>
2019-10-16 21:46:52 +0600

    Delete Min Steps in Infinite Grid.cpp

--- InterviewBit/Min Steps in Infinite Grid.cpp
+++
@@ -1,9 +0,0 @@
-int Solution::coverPoints(vector<int> &A, vector<int> &B) {
-    int ans =0;
-    for(int i =0; i<A.size()-1;i++)
-    {
-        pair<int,int> x={A[i],B[i]},y={A[i+1],B[i+1]};
-        ans+=max(abs(x.first-y.first),abs(x.second-y.second));
-    }
-    return ans;
-}
