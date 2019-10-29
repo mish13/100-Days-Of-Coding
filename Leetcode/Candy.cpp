ba29828c2fdd55e5e1f364cfbba8093e0e31c89b
A. K. M. Fakhrul Hossain <ahmishal90@gmail.com>
2019-10-29 20:49:04 +0600

    Update Candy.cpp

--- Leetcode/Candy.cpp
+++ Leetcode/Candy.cpp
@@ -1,80 +1,43 @@
+// Runtime: 28 ms, faster than 67.76% of C++ online submissions for Candy.
+// Memory Usage: 10.3 MB, less than 84.62% of C++ online submissions for Candy.
+
 class Solution {
 public:
-    void dfs(vector <int> adj[],int arr[],int vis[],int u)
-    {
-        vis[u]=1;
-        for(int i=0;i<adj[u].size();i++)
-        {
-            int v = adj[u][i];
-            arr[v]=max(arr[v],arr[u]+1);
-            dfs(adj,arr,vis,v);
-        }
-    }
-    
     int candy(vector<int>& ratings) {
         int n = int(ratings.size());
-        int arr[n],vis[n];
-        priority_queue < pair<int,int>, vector< pair<int,int> >, greater < pair<int,int> > > pq;
-        pair <int,int> pr;
-        vector <int> adj[n];
-        memset(arr,0,sizeof(arr));
-        memset(vis,0,sizeof(vis));
-        for(int i=0;i<ratings.size();i++)
-        {
-            pq.push({ratings[i],i});
-        }
-        for(int i = 0; i<n ; i++)
-        {
-            if(i>0)
-            {
-                if(i<n-1)
-                {
-                    if(ratings[i]<ratings[i-1])
-                    {
-                        adj[i].push_back(i-1);
-                    }
-                    if(ratings[i]<ratings[i+1])
-                    {
-                        adj[i].push_back(i+1);
-                    }
-                }
-                else if(i==n-1)
-                {
-                    if(ratings[i]<ratings[i-1])
-                    {
-                        adj[i].push_back(i-1);
-                    }
-                }
-            }
-            else if(i==0)
-            {
-                if(i<n-1)
-                {
-                    if(ratings[i]<ratings[i+1])
-                    {
-                        adj[i].push_back(i+1);
-                    }
-                }
-            }
-        }
-        while(!pq.empty())
-        {
-            pr = pq.top();
-            pq.pop();
-            if(vis[pr.second]==0)
-            {
-                arr[pr.second]=1;
-                vis[pr.second]=1;
-                dfs(adj,arr,vis,pr.second);
-            }
-            else
-                continue;
-        }
-        int ans = 0;
+        int left2right[n],right2left[n];
         for(int i=0;i<n;i++)
         {
-            ans+=arr[i];
-            //cout<<arr[i]<<endl;
+            if(i!=0)
+            {
+                if(ratings[i-1]<ratings[i])
+                {
+                    left2right[i]=left2right[i-1]+1;
+                }
+                else
+                    left2right[i]=1;
+            }
+            else
+                left2right[i]=1;
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i!=n-1)
+            {
+                if(ratings[i+1]<ratings[i])
+                {
+                    right2left[i]=right2left[i+1]+1;
+                }
+                else
+                    right2left[i]=1;
+            }
+            else
+                right2left[i]=1;
+        }
+        int ans =0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=max(left2right[i],right2left[i]);
         }
         return ans;
     }
